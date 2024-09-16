03e2d8f8fc88e14eace7753c9f42c19dd5345d86
UBCSG <gaosi4749us@gmail.com>
2024-09-16 12:44:53 -0400

    Update Figures

--- DataAnalysis/Plot.R
+++ DataAnalysis/Plot.R
@@ -1,14 +1,13 @@
 library(ggplot2)
 library(tidyverse)
-data.decay <- read.csv("DataAnalysis/Data/Cleaned_Data/decay2.csv")
-data.GLMM <- read.csv("DataAnalysis/Data/Cleaned_Data/GLMM2.csv")
+data.decay <- read.csv("Data/Cleaned_Data/decay.csv")
+data.GLMM <- read.csv("Data/Cleaned_Data/GLMM.csv")
 data.decay <- data.decay %>% 
   mutate(censor = ifelse(log10 == min(log10), 1, 0)) %>% 
   filter(! ((PATIENT == 26 & time > 13) | (PATIENT == 71 & time > 22.3)))
 
-data.10days <- data.decay %>% 
-  filter(time <= 10)
-p1 <- ggplot(data.10days, aes(x=time, y=log10)) + 
+# The viral load (in log10-scale) trajectories during treatment.
+p1 <- ggplot(data.decay, aes(x=time, y=log10)) + 
   geom_point(aes(shape=factor(censor)),size=2, stroke=0) +
   geom_line(aes(group=PATIENT)) +
   scale_x_continuous("Day") + 
@@ -28,5 +27,17 @@ p2 <- ggplot(subdata, aes(x=time, y=log10)) +
   scale_shape_manual(values=c(16, 17),labels = c("No", "Yes"))+
   labs(shape = "Censored") + 
   theme_classic()+theme(text=element_text(size=14),panel.grid.major=element_line(colour = "grey90"), panel.grid.minor =element_line(colour = "grey90"))
-
 ggarrange(p1, p2, common.legend = TRUE, legend = "right")
+
+# The viral load (in log10-scale) trajectories during treatment for the first 10 days.
+data.10days <- data.decay %>% 
+  filter(time <= 10)
+p1.10days <- ggplot(data.10days, aes(x=time, y=log10)) + 
+  geom_point(aes(shape=factor(censor)),size=2, stroke=0) +
+  geom_line(aes(group=PATIENT)) +
+  scale_x_continuous("Day") + 
+  scale_y_continuous(bquote("Viral load (in" ~ log[10]~"-scale)"))+
+  scale_shape_manual(values=c(16, 17),labels = c("No", "Yes"))+
+  labs(shape = "Censored") + 
+  theme_classic()+theme(text=element_text(size=14),panel.grid.major=element_line(colour = "grey90"), panel.grid.minor =element_line(colour = "grey90"))
+p1.10days
